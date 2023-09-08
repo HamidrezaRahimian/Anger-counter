691afc56a6993a16d8bd4358f6886ec523f18a5f
Alex <hamidreza.rahimian@emerson.com>
2023-09-08 11:19:17 +0200

    fnished version of anger counter in 125% size

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("slaaam")
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+let count = 0 ;
+function increment() {
+    count += 1 ;
+    countEl.innerText = count ;
+}
+
+function save() {
+    let space = " - "
+    saveEl.textContent += count + " - ";
+    countEl.textContent = 0 ;
+    return count= 0;
+    
+}
